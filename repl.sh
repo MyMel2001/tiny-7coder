13dbcfa52cc546e77d3723b19a7d57276467e054
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-07-16 09:59:09 +0000

    Update repl.sh

--- repl.sh
+++ repl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while true
 do
 read -p "request >" req
-python3 "t7c.py" "$req"
+"$SCRIPT_DIR/python3" "t7c.py" "$req"
 done
