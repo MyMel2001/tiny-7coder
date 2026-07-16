64669971153d8abfa2826e8dbcd2619e0bf1053c
MyMel2001 Holder <163201376+mymel2001-holder@users.noreply.github.com>
2026-07-16 10:04:30 +0000

    i am very fucking tired

--- repl.sh
+++ repl.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while true
 do
 read -p "request >" req
-"$SCRIPT_DIR/python3" "t7c.py" "$req"
+python3 "$SCRIPT_DIR/t7c.py" "$req"
 done
